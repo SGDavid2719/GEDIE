b309d41015802898055a4cc6a5038191943e0480
SGDavid2719 <santome.galvan.david@gmail.com>
2022-03-13 10:27:10 +0100

    portrait / cutom controls / some functions

--- PRACTICA_1/src/js/video.js
+++ PRACTICA_1/src/js/video.js
@@ -1,3 +1,41 @@
-$(document).ready(function () {
-	document.getElementById("videos-title").innerHTML = "TITLE MODIFIED";
-});
+// Select html elements by ID
+const lVideo = document.getElementById("video");
+const lVideoControls = document.getElementById("video-controls");
+
+const playbackIcons = document.querySelectorAll(".playback-icons use");
+
+// Gets if the video can be played by the user's browser
+const lVideoWorks = !!document.createElement("video").canPlayType;
+if (lVideoWorks) {
+	// Hides native controls
+	lVideo.controls = false;
+	lVideoControls.classList.remove("hidden");
+}
+
+// Play button
+const playButton = document.getElementById("play");
+// Play event listener
+//playButton.addEventListener("click", togglePlay());
+
+//lVideo.addEventListener("play", updatePlayButton);
+//lVideo.addEventListener("pause", updatePlayButton);
+
+// Functions
+
+function togglePlay() {
+	if (lVideo.paused || lVideo.ended) {
+		lVideo.play();
+	} else {
+		lVideo.pause();
+	}
+}
+
+function updatePlayButton() {
+	playbackIcons.forEach((icon) => icon.classList.toggle("hidden"));
+
+	if (lVideo.paused) {
+		playButton.setAttribute("data-title", "Play (k)");
+	} else {
+		playButton.setAttribute("data-title", "Pause (k)");
+	}
+}
