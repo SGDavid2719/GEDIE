a12b735dd00e9662d5ae295f7da1125a39d63ac1
SGDavid2719 <santome.galvan.david@gmail.com>
2022-03-13 13:01:07 +0100

    progress bar / volume / play-pause

--- PRACTICA_1/src/js/video.js
+++ PRACTICA_1/src/js/video.js
@@ -1,8 +1,7 @@
-// Select html elements by ID
+/* -------------------- Init -------------------- */
+
 const lVideo = document.getElementById("video");
 const lVideoControls = document.getElementById("video-controls");
-
-const playbackIcons = document.querySelectorAll(".playback-icons use");
 
 // Gets if the video can be played by the user's browser
 const lVideoWorks = !!document.createElement("video").canPlayType;
@@ -12,15 +11,49 @@ if (lVideoWorks) {
 	lVideoControls.classList.remove("hidden");
 }
 
-// Play button
-const playButton = document.getElementById("play");
-// Play event listener
-//playButton.addEventListener("click", togglePlay());
+//////////////////////////////////////////////////////////
+//					Play-Pause Buttons					//
+//////////////////////////////////////////////////////////
 
-//lVideo.addEventListener("play", updatePlayButton);
-//lVideo.addEventListener("pause", updatePlayButton);
+const lPlayButton = document.getElementById("play");
+lPlayButton.addEventListener("click", togglePlay);
+lVideo.addEventListener("play", updatePlayButton);
+lVideo.addEventListener("pause", updatePlayButton);
 
-// Functions
+//////////////////////////////////////////////////////////
+//						Time elapsed					//
+//////////////////////////////////////////////////////////
+
+const lTimeElapsed = document.getElementById("time-elapsed");
+const lDuration = document.getElementById("duration");
+lVideo.addEventListener("loadedmetadata", initializeVideo);
+lVideo.addEventListener("timeupdate", updateTimeElapsed);
+
+//////////////////////////////////////////////////////////
+//						Progress bar					//
+//////////////////////////////////////////////////////////
+
+const lProgressBar = document.getElementById("progress-bar");
+const lSeek = document.getElementById("seek");
+lVideo.addEventListener("timeupdate", updateProgress);
+const lSeekTooltip = document.getElementById("seek-tooltip");
+lSeek.addEventListener("mousemove", updateSeekTooltip);
+lSeek.addEventListener("input", skipAhead);
+
+//////////////////////////////////////////////////////////
+//							Volume						//
+//////////////////////////////////////////////////////////
+
+const lVolumeButton = document.getElementById("volume-btn");
+const lVolume = document.getElementById("volume");
+lVolume.addEventListener("input", updateVolume);
+lVideo.addEventListener("volumechange", updateVolumeIcon);
+
+/* -------------------- Functions -------------------- */
+
+//////////////////////////////////////////////////////////
+//					Play-Pause Buttons					//
+//////////////////////////////////////////////////////////
 
 function togglePlay() {
 	if (lVideo.paused || lVideo.ended) {
@@ -31,11 +64,106 @@ function togglePlay() {
 }
 
 function updatePlayButton() {
-	playbackIcons.forEach((icon) => icon.classList.toggle("hidden"));
-
 	if (lVideo.paused) {
-		playButton.setAttribute("data-title", "Play (k)");
+		$(".fa-pause").addClass("d-none");
+		$(".fa-play").removeClass("d-none");
+		document.querySelector("#play span").setAttribute("title", "Play (k)");
 	} else {
-		playButton.setAttribute("data-title", "Pause (k)");
+		$(".fa-pause").removeClass("d-none");
+		$(".fa-play").addClass("d-none");
+		document.querySelector("#play span").setAttribute("title", "Pause (k)");
+	}
+}
+
+//////////////////////////////////////////////////////////
+//						Time elapsed					//
+//////////////////////////////////////////////////////////
+
+function formatTime(lTimeInSeconds) {
+	const lResult = new Date(lTimeInSeconds * 1000).toISOString().substr(11, 8);
+
+	return {
+		minutes: lResult.substr(3, 2),
+		seconds: lResult.substr(6, 2),
+	};
+}
+
+function initializeVideo() {
+	const lVideoDuration = Math.round(lVideo.duration);
+	lSeek.setAttribute("max", lVideoDuration);
+	lProgressBar.setAttribute("max", lVideoDuration);
+	const lTime = formatTime(lVideoDuration);
+	lDuration.innerText = `${lTime.minutes}:${lTime.seconds}`;
+	lDuration.setAttribute("datetime", `${lTime.minutes}m ${lTime.seconds}s`);
+}
+
+function updateTimeElapsed() {
+	const lTime = formatTime(Math.round(lVideo.currentTime));
+	lTimeElapsed.innerText = `${lTime.minutes}:${lTime.seconds}`;
+	lTimeElapsed.setAttribute("datetime", `${lTime.minutes}m ${lTime.seconds}s`);
+}
+
+//////////////////////////////////////////////////////////
+//						Progress bar					//
+//////////////////////////////////////////////////////////
+
+function updateProgress() {
+	lSeek.value = Math.floor(lVideo.currentTime);
+	lProgressBar.value = Math.floor(lVideo.currentTime);
+}
+
+function updateSeekTooltip(pEvent) {
+	const lSkipTo = Math.round(
+		(pEvent.offsetX / pEvent.target.clientWidth) *
+			parseInt(pEvent.target.getAttribute("max"), 10)
+	);
+	lSeek.setAttribute("data-seek", lSkipTo);
+	const lTime = formatTime(lSkipTo);
+	lSeekTooltip.textContent = `${lTime.minutes}:${lTime.seconds}`;
+	const lRect = lVideo.getBoundingClientRect();
+	lSeekTooltip.style.left = `${pEvent.pageX - lRect.left}px`;
+}
+
+function skipAhead(pEvent) {
+	const skipTo = pEvent.target.dataset.seek
+		? pEvent.target.dataset.seek
+		: pEvent.target.value;
+	lVideo.currentTime = skipTo;
+	lProgressBar.value = skipTo;
+	lSeek.value = skipTo;
+}
+
+//////////////////////////////////////////////////////////
+//							Volume						//
+//////////////////////////////////////////////////////////
+
+function updateVolume() {
+	if (lVideo.muted) {
+		lVideo.muted = false;
+	}
+
+	lVideo.volume = lVideo.value;
+}
+
+// updateVolumeIcon updates the volume icon so that it correctly reflects
+// the volume of the video
+function updateVolumeIcon() {
+	lVolumeButton.setAttribute("data-title", "Mute (m)");
+
+	if (lVideo.muted || lVideo.volume === 0) {
+		$(".fa-volume-high").addClass("d-none");
+		$(".fa-volume-low").addClass("d-none");
+		$(".fa-volume-xmark").removeClass("d-none");
+		volumeButton.setAttribute("data-title", "Unmute (m)");
+	} else if (lVideo.volume > 0 && lVideo.volume <= 0.5) {
+		volumeLow.classList.remove("hidden");
+		$(".fa-volume-high").addClass("d-none");
+		$(".fa-volume-low").removeClass("d-none");
+		$(".fa-volume-xmark").addClass("d-none");
+	} else {
+		volumeHigh.classList.remove("hidden");
+		$(".fa-volume-high").removeClass("d-none");
+		$(".fa-volume-low").addClass("d-none");
+		$(".fa-volume-xmark").addClass("d-none");
 	}
 }
